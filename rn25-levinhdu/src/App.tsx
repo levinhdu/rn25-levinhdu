b7eeca8511ef0d6b2997d3de76d517660fecbcf2
levinhdu <duledule1914@gmail.com>
2022-10-07 09:14:23 +0700

    responsive

--- rn25-levinhdu/src/App.tsx
+++ rn25-levinhdu/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         {/* content */}
         <div className="mt-5">
           <div className="row">
-            <div className="col">
+            <div className="col-xxl-3 col-xl-6 col-lg-12 col-md-8">
               <div className='border-bottom'>
                 <p className="ms-4 mt-2">The Driver</p>
               </div>
@@ -26,14 +26,14 @@ function App() {
                 <p className="ms-4 mt-2">The End</p>
               </div>
             </div>
-            <div className="col col-5">
+            <div className="col-xxl-5 col-xl-6 col-lg col-md-8">
               <h3>The Walk</h3>
               <p>The walk to the Pulpit Rock will take you approximately two hours, give or take an hour depending
                 on the weather conditions and your physical shape.
               </p>
               <img src="https://www.covermore.co.uk/sites/covermore_uk/files/blog-images/norway_image1.jpg" alt="" />
             </div>
-            <div className="col">
+            <div className="col-xxl-3 col-xl-6 col-lg col-md-8">
               <div className="bg-info">
                 <div className="div">
                   <h3>What?</h3>
